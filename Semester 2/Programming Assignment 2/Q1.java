96a0a6f827b43dd696b4c627afed6e0c1e3c7981
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2022-07-19 13:07:49 +0530

    Update Q1.java

--- Semester 2/Programming Assignment 2/Q1.java
+++ Semester 2/Programming Assignment 2/Q1.java
@@ -1,1 +1,40 @@
+import java.util.Scanner;
 
+class Commission
+{
+	double sales;
+	
+	Commission(double s)
+	{
+		sales = s;
+	}
+	
+	public double getCommission()
+	{
+		if (sales<500)
+			return 0.02*sales;
+		else if (sales>=500 && sales<5000)
+			return 0.05*sales;
+		else
+			return 0.08*sales;
+	}
+}
+
+
+public class Q1 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter sales : ");
+		double s = sc.nextDouble();
+		Commission c = new Commission(s);
+		double com = c.getCommission();
+		
+		if (s<0)
+			System.out.println("Invalid Input!");
+		else
+			System.out.println("Commission : "+com);
+		sc.close();
+	}
+}
