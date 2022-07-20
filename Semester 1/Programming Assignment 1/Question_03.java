7df7890a5cc80df98f39c352d57d68239dc8b45f
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2022-07-20 22:15:30 +0530

    Add files via upload

---
+++ Semester 1/Programming Assignment 1/Question_03.java
@@ -0,0 +1,28 @@
+// Name: Soumyadip Majumder
+// Section: 11
+// Registration no.: 2141013055
+// Problem description: Display initial of your name in block letters.
+
+public class Question_03 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("                   S               M                      M");
+		System.out.println("                S                  M M                   MM");
+		System.out.println("             S                     M   M               M  M");
+		System.out.println("           S                       M     M           M    M");
+		System.out.println("           S                       M       M       M      M");
+		System.out.println("             S                     M         M   M        M");
+		System.out.println("                 S                 M           M          M");
+		System.out.println("                   S               M                      M");
+		System.out.println("                    S              M                      M");
+		System.out.println("                    S              M                      M");
+		System.out.println("                   S               M                      M");
+		System.out.println("                 S                 M                      M");
+		System.out.println("              S                    M                      M");
+		System.out.println("           S                       M                      M");
+	
+
+	}
+
+}
