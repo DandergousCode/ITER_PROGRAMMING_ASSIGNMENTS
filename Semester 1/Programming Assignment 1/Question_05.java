bfa284ffaea61639b8101b0dde9bcbb40d1a31ee
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2022-07-20 22:10:41 +0530

    Add files via upload

---
+++ Semester 1/Programming Assignment 1/Question_05.java
@@ -0,0 +1,16 @@
+// Name: Soumyadip Majumder
+// Section: 11
+// Registration no.: 2141013055
+// Problem description: Stores Regd. No and year into two variables, and displays their values on the screen.
+
+public class Question_05 
+{
+
+	public static void main(String[] args) 
+	{
+		int registration_no = 2141013055;
+		int year = 2021;
+		System.out.print("My Regd. No is " + registration_no + " and I have taken admission in B.Tech in " + year + ".");
+	}
+
+}
---
+++ Semester 1/Programming Assignment 1/Question_13.java
@@ -0,0 +1,26 @@
+// Name: Soumyadip Majumder
+// Section: 11
+// Registration no.: 2141013055
+// Problem description: Assume a string variable rurler1 contains"1" initially i.e.
+//                         String ruler1 = "1"
+//                     Write a java program to print the following output using string concatenation.
+
+public class Question_13 
+{
+	public static void main(String[] args) 
+	{
+		String ruler1 = " 1 ";
+		System.out.println(ruler1);
+		
+		ruler1 = ruler1 + 2 + ruler1;
+		System.out.println(ruler1);
+		
+		ruler1 = ruler1 + 3 + ruler1;
+		System.out.println(ruler1);
+		
+		ruler1 = ruler1 + 4 + ruler1;
+		System.out.println(ruler1);
+
+	}
+
+}
