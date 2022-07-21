1576f73134cefb1a0742f43a404076243ee1b2c4
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2022-07-21 22:52:54 +0530

    Add files via upload

---
+++ Semester 1/Programming Assignment 2/Assignment_16_CLA_03.java
@@ -0,0 +1,18 @@
+// Name: Soumyadip Majumder
+// Section: 11
+// Regd.no: 2141013055
+// Problem: Command Line Arguments.
+public class Assignment_16_CLA_03 
+{
+    public static void main(String[] args) 
+    {
+        int sides = 6;
+        int a = 1 + (int)(Math.random() * sides);
+        int b = 1 + (int)(Math.random() * sides);
+
+        int sum = a + b;
+        System.out.println(sum);
+        
+    }
+    
+}
