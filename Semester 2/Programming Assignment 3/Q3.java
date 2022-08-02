2630ae408cd7a42082d19a8327333431b4c0b5b1
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2022-08-02 09:39:37 +0530

    Update Q3.java

--- Semester 2/Programming Assignment 3/Q3.java
+++ Semester 2/Programming Assignment 3/Q3.java
@@ -1,4 +1,40 @@
+import java.util.Scanner;
+class Student
+{
+	static String name;
+	static int mark;
+	void input()
+	{
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		try
+		{
+			System.out.print("Enter name : ");
+			name = sc.nextLine();
+			System.out.print("Enter marks : ");
+			mark = sc.nextInt();
+			
+			if (mark>100)
+				throw new Exception("MarksOutOfBoundException: ");
+		}catch(Exception e) {System.out.println(e.getMessage()+"Invalid marks input");}
+	}
+	void output()
+	{
+		System.out.println("NAME : "+name);
+		if (mark<=100)
+			System.out.println("MARKS : "+mark+" out of 100");
+		else
+			System.out.println("INVALID MARKS INPUT!");
+	}
+}
+
 
 public class Q3 
 {
+	public static void main(String[] args) 
+	{
+		Student s = new Student();
+		s.input();
+		s.output();
+	}
 }
