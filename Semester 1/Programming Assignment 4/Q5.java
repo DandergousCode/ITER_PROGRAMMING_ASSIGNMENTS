9140730f4c3b8a99b9bf3bca23c1fbd9c58a7875
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2022-08-23 03:40:45 +0530

    Add files via upload

---
+++ Semester 1/Programming Assignment 4/Q5.java
@@ -0,0 +1,10 @@
+public class Q5 
+{
+    public static void main(String[] args) 
+    {
+        for (float i=-2; i<=2;)
+        {
+            System.out.println(i);  i=i+0.5f;
+        }
+    }
+}
