80507dcbdaf922888142d57acfb4df6d0d4352db
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2022-08-23 15:12:43 +0530

    Add files via upload

---
+++ Semester 1/Programming Assignment 4/Q8.java
@@ -0,0 +1,12 @@
+public class Q8 
+{
+    public static void main(String[] args) 
+    {
+        String a="1";
+        for (int i=2; i<=6; i++)
+        {
+            System.out.println(a);
+            a=a+i+a;
+        }
+    }    
+}
